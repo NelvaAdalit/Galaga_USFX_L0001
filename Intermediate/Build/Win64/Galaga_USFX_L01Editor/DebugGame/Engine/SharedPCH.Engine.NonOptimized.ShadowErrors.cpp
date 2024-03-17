f0999ac1cf348c86320a4842254078c1bf163872
NelvaAdalit <moraadalid378@gmail.com>
2024-03-16 20:12:14 -0400

    cambioss

---
+++ Intermediate/Build/Win64/Galaga_USFX_L01Editor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30154
+#include "C:/Users/CAPITAL COMPUTERS/source/repos/Galaga_USFX_L0001/Intermediate/Build/Win64/Galaga_USFX_L01Editor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.h"
--- Intermediate/Build/Win64/Galaga_USFX_L01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Intermediate/Build/Win64/Galaga_USFX_L01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.29.30154
-#include "C:/Users/CAPITAL COMPUTERS/Downloads/Galaga_USFX_L02 Grupo 1-master/Galaga_USFX_L01-master/Intermediate/Build/Win64/Galaga_USFX_L01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/CAPITAL COMPUTERS/source/repos/Galaga_USFX_L0001/Intermediate/Build/Win64/Galaga_USFX_L01Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
